f3b483ba80a040de561d109b269e9535e1a29276
petermbiyu <petersmasha@gmail.com>
2025-07-21 05:28:41 +0300

    first commit to quiz app

---
+++ script.js
@@ -0,0 +1,59 @@
+const questions = [
+  {
+    question: "Who was the first president of kenya",
+    answers: [
+      { answer: "Uhuru Kenyata", correct: "false" },
+      { answer: "Mwai Kibaki", correct: "false" },
+      { answer: "Jomo Kenyata", correct: "true" },
+      { answer: "William Ruto", correct: "false" },
+    ],
+  },
+  {
+    question: "What is the capital city of Kenya",
+    answers: [
+      { answer: "Dondoma", correct: "false" },
+      { answer: "Nairobi", correct: "true" },
+      { answer: "Adis Ababa", correct: "false" },
+      { answer: "Mogadishu", correct: "false" },
+    ],
+  },
+  {
+    question: "When did kenya gain its independence and became a republic",
+    answers: [
+      { answer: "1963", correct: "true" },
+      { answer: "1956", correct: "false" },
+      { answer: "1972", correct: "false" },
+      { answer: "1985", correct: "false" },
+    ],
+  },
+  {
+    question:
+      "The following statements are true about Kenya. Which one is not?",
+    answers: [
+      {
+        answer: "Mt. Kenya is located in central region of Kenya",
+        correct: "false",
+      },
+      {
+        answer:
+          "Mombasa serves as a gateway to kenya and EastAfrica through its port",
+        correct: "false",
+      },
+      { answer: "Lake Victorory is a fresh water lake", correct: "false" },
+      {
+        answer: "River Nile drains its water into Lake Victoria",
+        correct: "true",
+      },
+    ],
+  },
+  {
+    question:
+      "What is the name of the largest ethnic group by population in Kenya",
+    answers: [
+      { answer: "Kalenjin", correct: "false" },
+      { answer: "Luo", correct: "false" },
+      { answer: "Kikuyu", correct: "true" },
+      { answer: "Minji Kenda", correct: "false" },
+    ],
+  },
+];
